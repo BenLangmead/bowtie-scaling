11390e801da89f12faf7ede42c34c8109eb7320a
Valentin Antonescu <valentin@jhu.edu>
2015-05-15 14:19:11 -0400

    Replaced the command line comma style with a subprocess to avoid hitting the command line lenght limit.

--- thread_scaling/scripts/experiments/run_normal.sh
+++ thread_scaling/scripts/experiments/run_normal.sh
@@ -2,14 +2,13 @@
 
 cmd_templ="/home/vanton/work/bowtie2/bowtie2-align-s -x /home/vanton/work/hg19/hg19 -U "
 fastq_file="seqs_by_100.fq"
+sub_proc_templ_1="<(for ((i=0;i<"
+sub_proc_templ_2=";i++)); do cat $fastq_file; done)"
+
 data_templ=$1
 
 for ((t=1; t<40; t++)); do
-  f_file=$fastq_file
-  for (( i=1; i<t; i++ )); do
-    f_file+=",$f_file"
-  done
-  cmd="$cmd_templ $f_file -p $t "
+  cmd="$cmd_templ ${sub_proc_templ_1}${t}${sub_proc_templ_2} -p $t "
   data_file="${data_templ}${t}.out"
   echo $cmd
   $cmd | grep "thread:" > $data_file
