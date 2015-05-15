9a15db52f2c4ec19157ca9b8b32b8c26d307721e
Valentin Antonescu <valentin@jhu.edu>
2015-05-15 15:07:31 -0400

    Fixed bowtie2 threads param.

--- thread_scaling/scripts/experiments/run_normal.sh
+++ thread_scaling/scripts/experiments/run_normal.sh
@@ -8,6 +8,6 @@ data_templ=$1
 for ((t=1; t<40; t++)); do
   data_file="${data_templ}${t}.out"
   echo "threads: " $t
-  $cmd_templ <(for ((i=0;i<${t};i++)); do cat $fastq_file; done) | grep "thread:" > $data_file
+  $cmd_templ <(for ((i=0;i<${t};i++)); do cat $fastq_file; done) -p $t | grep "thread:" > $data_file
 done
 
