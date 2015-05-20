7958f0412b7137dd1688e5bb759ef805367d809f
BenLangmead <ben.langmead@gmail.com>
2015-05-20 14:05:07 -0400

    make min/max/avg as well

--- thread_scaling/scripts/scale_summary.sh
+++ thread_scaling/scripts/scale_summary.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-python scale_summary.py --in b2_scale_data/no_IO_* --ignore-above 24 --scatter thread_times_no_IO.tsv
-python scale_summary.py --in b2_scale_data/no_input_* --ignore-above 24 --scatter thread_times_no_I.tsv
-python scale_summary.py --in b2_scale_data/normal_sp_* --ignore-above 24 --scatter thread_times_default.tsv
+python scale_summary.py --in b2_scale_data/no_IO_* --ignore-above 24 --scatter thread_times_no_IO.tsv --min-max-avg min_max_avg_no_IO.tsv
+python scale_summary.py --in b2_scale_data/no_input_* --ignore-above 24 --scatter thread_times_no_I.tsv --min-max-avg min_max_avg_no_I.tsv
+python scale_summary.py --in b2_scale_data/normal_sp_* --ignore-above 24 --scatter thread_times_default.tsv --min-max-avg min_max_avg_default.tsv
