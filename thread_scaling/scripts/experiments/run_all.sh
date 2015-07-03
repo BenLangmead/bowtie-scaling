bcb43700a40c6ee12ce61d0c493c3cb5b6252ac2
BenLangmead <langmea@cs.jhu.edu>
2015-07-03 15:48:25 -0400

    adjusting to new macros

--- thread_scaling/scripts/experiments/run_all.sh
+++ thread_scaling/scripts/experiments/run_all.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+HG19_INDEX=/home/vanton/work/hg19/hg19
+MAX_THREADS=24
+
 # TODO: this is ugly but better ugly than more complicated parameters
-cmd_tmpl="./bowtie2-align-s -x /home/vanton/work/hg19/hg19 -U seqs_by_100.fq "
+cmd_tmpl="./bowtie2-align-s -x $HG19_INDEX -U seqs_by_100.fq "
 
 run_th () {
-for ((t=1; t<24; t++)); do
+for ((t=1; t<$MAX_THREADS; t++)); do
   cmd="$cmd_templ $t "
   data_file="./runs/${1}${t}.out"
   echo $cmd
@@ -27,18 +30,17 @@ mkdir -p runs
 #start with normal 
 git checkout master
 rm bowtie2-align-s
-make -j bowtie2-align-s
+make EXTRA_FLAGS="-DUSE_FINE_TIMER -DPER_THREAD_TIMING" bowtie2-align-s
 run_th normal_
 
 # Only no input sync
 git checkout no_in_sync
 rm bowtie2-align-s
-make -j bowtie2-align-s
+make EXTRA_FLAGS="-DUSE_FINE_TIMER -DPER_THREAD_TIMING" bowtie2-align-s
 run_th no_in_
 
 # No input/output sync
 git checkout no_IO_2000seq
 rm bowtie2-align-s
-make -j bowtie2-align-s
+make EXTRA_FLAGS="-DUSE_FINE_TIMER -DPER_THREAD_TIMING" bowtie2-align-s
 run_th no_io_
-
