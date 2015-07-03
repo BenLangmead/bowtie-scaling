eabcd8c27b01e4273488eb7ec4a6be47bf96c7fb
BenLangmead <langmea@cs.jhu.edu>
2015-07-03 13:51:14 -0400

    removed jitter

--- thread_scaling/scripts/scale_summary_plot.R
+++ thread_scaling/scripts/scale_summary_plot.R
@@ -1,6 +1,8 @@
+jitter_factor <- 0
+
 pdf(file='thread_times_no_I.pdf')
 xy <- read.table('thread_times_no_I.tsv')
-plot(xy$V1, jitter(xy$V2, factor=3), ylim=c(0, 160),
+plot(xy$V1, jitter(xy$V2, factor=jitter_factor), ylim=c(0, 160),
 	    xlab='# simultaneous threads',
 	    ylab='Running time',
 	    main='Running time for each thread, no input locking')
@@ -8,7 +10,7 @@ dev.off()
 
 pdf(file='thread_times_no_IO.pdf')
 xy <- read.table('thread_times_no_IO.tsv')
-plot(xy$V1, jitter(xy$V2, factor=3), ylim=c(0, 160),
+plot(xy$V1, jitter(xy$V2, factor=jitter_factor), ylim=c(0, 160),
 	    xlab='# simultaneous threads',
 	    ylab='Running time',
 	    main='Running time for each thread, no input or output locking')
@@ -16,7 +18,7 @@ dev.off()
 
 pdf(file='thread_times_default.pdf')
 xy <- read.table('thread_times_default.tsv')
-plot(xy$V1, jitter(xy$V2, factor=3), ylim=c(0, 160),
+plot(xy$V1, jitter(xy$V2, factor=jitter_factor), ylim=c(0, 160),
 	    xlab='# simultaneous threads',
 	    ylab='Running time',
 	    main='Running time for each thread, default bowtie')
