c4560ceb90c92b11515337d455a3d119eeb2b3f3
BenLangmead <langmea@cs.jhu.edu>
2015-07-05 11:34:44 -0400

    for ec2 experiments

---
+++ thread_scaling/scripts/run_ec2_instance.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+#
+# DON'T FORGET TO SHUT IT DOWN WHEN YOU'RE DONE
+#
+
+aws ec2 run-instances \
+    --dry-run \
+    --image-id ami-1ecae776 \
+    --key-name gsg-keypair \
+    --instance-type c4.8xlarge \
+    --placement AvailabilityZone=us-east-1c \
+    --count 1
---
+++ thread_scaling/scripts/run_ec2_spot_instance.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+#
+# DON'T FORGET TO SHUT IT DOWN WHEN YOU'RE DONE
+#
+
+aws ec2 request-spot-instances \
+    --spot-price "0.500" \
+    --instance-count 1 \
+    --type "one-time" \
+    --launch-specification "{\"ImageId\":\"ami-1ecae776\",\"InstanceType\":\"c4.8xlarge\",\"Placement\":{\"AvailabilityZone\":\"us-east-1c\"}}"
+
+echo "Use \"aws ec2 describe-instances\" to check on your reservation"
+echo "Don't forget to shut it down when you're done"
