50c6587fd3cf9d2b08cc3165ab1f054ddbe37d74
Valentin Antonescu <valentin@jhu.edu>
2015-08-10 13:56:52 -0400

    More clear and usable vtune script.

--- thread_scaling/scripts/experiments/vtune_run.sh
+++ thread_scaling/scripts/experiments/vtune_run.sh
@@ -1,54 +1,14 @@
 #!/bin/bash
 
+for thread in 1 12 24 52 100 120; do
+    amplxe-cl -collect general-exploration \
+      -app-working-dir $HOME/bowtie-scaling/thread_scaling/scripts/experiments \
+      -- ./bowtie2-align-s-no-io \
+         -x $HOME/data/hg19 \
+         -U seqs_by_100.fq \
+         -p $thread \
+         -S /dev/null
 
-amplxe-cl -collect general-exploration \
-  -app-working-dir $HOME/bowtie-scaling/thread_scaling/scripts/experiments \
-  -- ./bowtie2-align-s-no-io \
-     -x $HOME/data/hg19 \
-     -U seqs_by_100.fq \
-     -p 1 \
-     -S /dev/null
-
-amplxe-cl -collect general-exploration \
-  -app-working-dir $HOME/bowtie-scaling/thread_scaling/scripts/experiments \
-  -- ./bowtie2-align-s-no-io \
-     -x $HOME/data/hg19 \
-     -U seqs_by_100.fq \
-     -p 12 \
-     -S /dev/null
-  
-amplxe-cl -collect general-exploration \
-  -app-working-dir $HOME/bowtie-scaling/thread_scaling/scripts/experiments \
-  -- ./bowtie2-align-s-no-io \
-     -x $HOME/data/hg19 \
-     -U seqs_by_100.fq \
-     -p 24 \
-     -S /dev/null
-
-amplxe-cl -collect general-exploration \
-  -app-working-dir $HOME/bowtie-scaling/thread_scaling/scripts/experiments \
-  -- ./bowtie2-align-s-no-io \
-     -x $HOME/data/hg19 \
-     -U seqs_by_100.fq \
-     -p 52 \
-     -S /dev/null
-
-
-amplxe-cl -collect general-exploration \
-  -app-working-dir $HOME/bowtie-scaling/thread_scaling/scripts/experiments \
-  -- ./bowtie2-align-s-no-io \
-     -x $HOME/data/hg19 \
-     -U seqs_by_100.fq \
-     -p 100 \
-     -S /dev/null
-
-amplxe-cl -collect general-exploration \
-  -app-working-dir $HOME/bowtie-scaling/thread_scaling/scripts/experiments \
-  -- ./bowtie2-align-s-no-io \
-     -x $HOME/data/hg19 \
-     -U seqs_by_100.fq \
-     -p 120 \
-     -S /dev/null
 
 
 
