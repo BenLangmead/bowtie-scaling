bd05ba6fade6ec32c2babdcc99077dcf7fb378f5
Valentin Antonescu <valentin@jhu.edu>
2015-08-11 13:18:39 -0400

    Updated vtune script.

--- thread_scaling/scripts/experiments/vtune_run.sh
+++ thread_scaling/scripts/experiments/vtune_run.sh
@@ -8,8 +8,17 @@ for thread in 1 12 24 52 100 120; do
          -U seqs_by_100.fq \
          -p $thread \
          -S /dev/null
+done
 
-
+for thread in 1 12 24 ; do
+    amplxe-cl -collect general-exploration \
+      -app-working-dir $HOME/bowtie-scaling/thread_scaling/scripts/experiments \
+      -- numactl --cpunodebind=0 --membind=0 ./bowtie2-align-s-no-io \
+         -x $HOME/data/hg19 \
+         -U seqs_by_100.fq \
+         -p $thread \
+         -S /dev/null
+done
 
 
 
