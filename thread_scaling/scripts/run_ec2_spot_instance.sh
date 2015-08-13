bea0ccb490d42751440c8adc5bf0bb9d48cfb3a5
BenLangmead <langmea@cs.jhu.edu>
2015-08-13 11:41:23 -0400

    switched to 1a AZ, made bid price parameter

--- thread_scaling/scripts/run_ec2_spot_instance.sh
+++ thread_scaling/scripts/run_ec2_spot_instance.sh
@@ -4,11 +4,22 @@
 # DON'T FORGET TO SHUT IT DOWN WHEN YOU'RE DONE
 #
 
+# us-east-1a subnet: subnet-d239b18b
+# us-east-1b subnet: 
+# us-east-1c subnet: subnet-5b76c570
+# us-east-1d subnet: subnet-e77d0890
+# us-east-1e subnet: subnet-aadc8790
+
+if [ -z "$1" ] ; then
+    echo "Must specify spot bid.  0.5 (50 cents) recommended"
+    exit 1
+fi
+
 aws ec2 request-spot-instances \
-    --spot-price "0.500" \
+    --spot-price "$1" \
     --instance-count 1 \
     --type "one-time" \
-    --launch-specification "{\"KeyName\":\"default\",\"ImageId\":\"ami-1ecae776\",\"InstanceType\":\"c4.8xlarge\",\"Placement\":{\"AvailabilityZone\":\"us-east-1c\"},\"NetworkInterfaces\":[{\"DeviceIndex\":0,\"SubnetId\":\"subnet-5b76c570\"}]}"
+    --launch-specification "{\"KeyName\":\"default\",\"ImageId\":\"ami-1ecae776\",\"InstanceType\":\"c4.8xlarge\",\"Placement\":{\"AvailabilityZone\":\"us-east-1a\"},\"NetworkInterfaces\":[{\"DeviceIndex\":0,\"SubnetId\":\"subnet-d239b18b\"}]}"
 
 echo "Use \"aws ec2 describe-instances\" to check on your reservation"
 echo "Don't forget to shut it down when you're done"
