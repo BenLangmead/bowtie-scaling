4ab7c9ed187d88a5ed7bf127c925d2d62cc21cd3
BenLangmead <langmea@cs.jhu.edu>
2015-09-02 11:35:14 -0400

    first attempt at nodebind script

---
+++ thread_scaling/scripts/experiments/nodebind_test.sh
@@ -0,0 +1,101 @@
+#!/bin/bash
+
+
+#!/bin/bash
+
+HG19_INDEX=$HOME/hg19
+MAX_THREADS=`grep 'processor\s*:' /proc/cpuinfo | wc -l`
+DR=`dirname $0`
+READS="$DR/seqs_by_100.fq"
+cmd_tmpl="-x $HG19_INDEX "
+NODES=`numactl --hardware | grep available: | awk '{print $2}'`
+
+echo "Max threads = $MAX_THREADS"
+
+run_th () {
+for mode in very-fast fast sensitive very-sensitive ; do
+  nthread=1
+  for ((t=$NODES; t<=$MAX_THREADS; t+=$NODES)); do
+    cmd="./${1} $cmd_tmpl --$mode -U "
+    mkdir -p numabind_runs/$mode
+    mkdir -p ../../../results/elephant6/numabind_raw/$mode
+    data_file="../../../results/elephant6/numabind_raw/$mode/${2}${t}.out"
+    echo "mode: $mode, threads: $t"
+    echo "Concatenating input reads"
+    for ((i=0; i<$NODES; i++)); do
+        cp $READS /tmp/.nodebind_test_reads_${i}.fq
+        for ((i=1;i<${t};i+=$NODES)); do cat $READS >> /tmp/.nodebind_test_reads_${i}.fq; done
+    done
+    # make sure input and output are on a local filesystem, not NFS
+    echo "Running bowtie2"
+    pids=""
+    for ((i=0; i<$NODES; i++)); do
+        numactl -N $i $cmd /tmp/.nodebind_test_reads_${i}.fq -p $nthread -S /tmp/.nodebind_test_reads_${i}.sam | grep "thread:" > $data_file &
+        echo "  spawned node $i process with pid $!"
+        pids="$! $pids"
+    done
+    for pid in $pids ; do
+        echo "  waiting for PID $pid"
+        wait $pid
+    done
+    ((nthread++))
+  done
+done
+}
+
+if [[ $# < 1 ]]; then
+  echo -e "Run all experiments.\n"
+  echo -e "\t$0 [bowtie-extra-parameters]"
+  echo "This will run all bowtie2 experiments and store the timing for each thread into ./runs/[experiment]_[n].out"
+  echo "where [n] will be replaced with the number of threads used for bowtie2 -p parameter."
+  exit 0
+fi
+cmd_tmpl="$cmd_tmpl $1"
+
+# Normal (all synchronization enabled), no TBB
+if [ ! -f "bowtie2-align-s-master" ] ; then
+  git checkout master
+  rm -f bowtie2-align-s-master bowtie2-align-s
+  make WITH_THREAD_PROFILING=1 EXTRA_FLAGS="-DUSE_FINE_TIMER" bowtie2-align-s
+  mv bowtie2-align-s bowtie2-align-s-master
+fi
+run_th bowtie2-align-s-master normal_
+
+# Normal (all synchronization enabled), with TBB
+if [ ! -f "bowtie2-align-s-master-tbb" ] ; then
+  git checkout master
+  rm -f bowtie2-align-s-master-tbb bowtie2-align-s
+  make WITH_THREAD_PROFILING=1 EXTRA_FLAGS="-DUSE_FINE_TIMER" WITH_TBB=1 bowtie2-align-s
+  mv bowtie2-align-s bowtie2-align-s-master-tbb
+fi
+run_th bowtie2-align-s-master-tbb normaltbb_
+
+# Normal (all synchronization enabled), with TBB and thread affinitization
+if [ ! -f "bowtie2-align-s-master-tbb-pin" ] ; then
+  git checkout affinitize
+  rm -f bowtie2-align-s-master-tbb-pin bowtie2-align-s
+  make WITH_THREAD_PROFILING=1 EXTRA_FLAGS="-DUSE_FINE_TIMER" WITH_TBB=1 WITH_AFFINITY=1 bowtie2-align-s
+  mv bowtie2-align-s bowtie2-align-s-master-tbb-pin
+fi
+run_th bowtie2-align-s-master-tbb-pin normaltbbpin_
+
+# Only no input sync
+if [ ! -f "bowtie2-align-s-no-in-sync" ] ; then
+  git checkout no_in_sync
+  rm -f bowtie2-align-s-no-in-sync bowtie2-align-s
+  make WITH_THREAD_PROFILING=1 EXTRA_FLAGS="-DUSE_FINE_TIMER" bowtie2-align-s
+  mv bowtie2-align-s bowtie2-align-s-no-in-sync
+fi
+run_th bowtie2-align-s-no-in-sync no_in_
+
+# No input/output sync
+if [ ! -f "bowtie2-align-s-no-io" ] ; then
+  git checkout no_IO_2000seq
+  rm -f bowtie2-align-s-no-io bowtie2-align-s
+  make WITH_THREAD_PROFILING=1 EXTRA_FLAGS="-DUSE_FINE_TIMER" bowtie2-align-s
+  mv bowtie2-align-s bowtie2-align-s-no-io
+fi
+run_th bowtie2-align-s-no-io no_io_
+
+
+--cpunodebind=nodes
