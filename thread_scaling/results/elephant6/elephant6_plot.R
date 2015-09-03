ef7733412bd7f50df97f3898cc17c8c48adaaafb
BenLangmead <langmea@cs.jhu.edu>
2015-09-02 21:40:56 -0400

    add series so we can see difference between sync/unsync for the different sensitivity levels

--- thread_scaling/results/elephant6/elephant6_plot.R
+++ thread_scaling/results/elephant6/elephant6_plot.R
@@ -1,10 +1,9 @@
-jitter_factor <- 0
 prefix <- ''
 
 # Scatter plot of thread running times for no input or output sync
 pdf(file=paste(prefix, 'elephant6_sensitive_no_io.pdf', sep=''))
 xy <- read.table(paste(prefix, 'sensitive_no_io.tsv', sep=''))
-plot(xy$V1, jitter(xy$V2, factor=jitter_factor), ylim=c(0, 110),
+plot(xy$V1, xy$V2, ylim=c(0, 110),
 	xlab='# simultaneous threads',
 	ylab='Running time',
 	main='Running time for each thread, no input or output locking')
@@ -13,7 +12,7 @@ dev.off()
 # Scatter plot of thread running times for no input or output sync
 pdf(file=paste(prefix, 'elephant6_sensitive_no_io_rescaled.pdf', sep=''))
 xy <- read.table(paste(prefix, 'sensitive_no_io.tsv', sep=''))
-plot(xy$V1, jitter(xy$V2, factor=jitter_factor),
+plot(xy$V1, xy$V2,
 	xlab='# simultaneous threads',
 	ylab='Running time',
 	main='Running time for each thread, no input or output locking')
@@ -29,7 +28,7 @@ plot(xy_def_mm$V4, type="l", ylim=c(0, 450), col='red', lwd=2,
 		ylab='Running time',
 		main='Running time for each thread, no input or output locking')
 lines(xy_noio_mm$V4, type="l", lwd=2)
-points(xy_noio$V1, jitter(xy_noio$V2, factor=jitter_factor))
+points(xy_noio$V1, xy_noio$V2)
 dev.off()
 
 # Series of 4 plots, all with same y axis showing the 4 different sensitivities
@@ -37,9 +36,24 @@ pdf(file=paste(prefix, 'elephant6_series_no_io_series.pdf', sep=''), height=4, w
 par(mfrow=c(1,4))
 for(sens in c('very-fast', 'fast', 'sensitive', 'very-sensitive')) {
 	xy <- read.table(paste(prefix, sens, '_no_io.tsv', sep=''))
-	plot(xy$V1, jitter(xy$V2, factor=jitter_factor), ylim=c(30, 200), col=rgb(0, 0, 0, 0.2),
+	plot(xy$V1, xy$V2, ylim=c(30, 200), col=rgb(0, 0, 0, 0.2),
 		xlab='# simultaneous threads',
 		ylab='Running time',
 		main=sens)
+}
+dev.off()
+
+# Series of 4 plots, all with same y axis showing the 4 different sensitivities
+pdf(file=paste(prefix, 'elephant6_series_no_io_and_avg_series.pdf', sep=''), height=4, width=14)
+par(mfrow=c(1,4))
+for(sens in c('very-fast', 'fast', 'sensitive', 'very-sensitive')) {
+	xy_noio <- read.table(paste(prefix, sens, '_no_io.tsv', sep=''))
+	xy_def_mm <- read.table(paste(prefix, 'avg_', sens, '_normal.tsv', sep=''))
+	xy_noio_mm <- read.table(paste(prefix, 'avg_', sens, '_no_io.tsv', sep=''))
+	plot(xy_def_mm$V4, type="l", ylim=c(0, 450), col='red', lwd=2,
+			xlab='# simultaneous threads',
+			ylab='Running time')
+	points(xy_noio$V1, xy_noio$V2, col=rgb(0, 0, 0, 0.1))
+	lines(xy_noio_mm$V4, type="l", lwd=2, col="blue")
 }
 dev.off()
