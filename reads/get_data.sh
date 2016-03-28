2658a52aa64bc4af1eeb68c0578e9713f647547f
BenLangmead <ben.langmead@gmail.com>
2016-03-28 08:34:49 -0400

    added HISAT data

--- reads/get_data.sh
+++ reads/get_data.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/env sh
 
-# Human reads from 1000 Genomes project
+# Human whole genome DNA sequencing reads from 1000 Genomes project
 # ERR050082 has 42,245,074 paired-end 100 x 100 nt reads
 # ERR050083 has 66,391,067 paired-end 100 x 100 nt reads
 
+# Human RNA sequencing reads from GEUVADIS
+# SRR1216135 has 10,908,030 paired-end 100 x 100 nt reads
+
+# http://www.ebi.ac.uk/ena/data/view/SRR1216135
+
+# WGS DNA-seq data for bowtie/bowtie2
 for d in ERR050082 ERR050083 ; do
 
     pref=`echo ${d} | sed 's/\(......\).*/\1/'`
@@ -13,4 +19,11 @@ for d in ERR050082 ERR050083 ; do
             curl ftp://ftp.sra.ebi.ac.uk/vol1/fastq/${pref}/${d}/${d}_${m}.fastq.gz | gzip -dc | head -n 1000000 > ${d}_${m}.fastq
         fi
     done
+done
+
+# RNA-seq data for HISAT
+for m in 1 2 ; do
+    if [ ! -f "SRR1216135_100k_${m}.fastq" ] ; then
+        curl ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR121/005/SRR1216135/SRR1216135_${m}.fastq.gz | gzip -dc | head -n 4000000 > SRR1216135_100k_${m}.fastq
+    fi
 done
--- reads/sample.sh
+++ reads/sample.sh
@@ -1,12 +1,25 @@
 #!/usr/bin/env sh
 
+# WGS DNA-seq data for bowtie/bowtie2
 for d in ERR050082 ERR050083 ; do
     for m in 1 2 ; do
-        python fastq_sample.py \
-            --in ${d}_${m}.fastq \
-            --out ${d}_${m}.sample10k.fastq \
-            --seed 72436 \
-            --n 10000
+        if [ ! -f "${d}_${m}.sample10k.fastq" ] ; then
+            python fastq_sample.py \
+                --in ${d}_${m}.fastq \
+                --out ${d}_${m}.sample10k.fastq \
+                --seed 72436 \
+                --n 10000
+        fi
     done
 done
 
+# RNA-seq data for HISAT
+for m in 1 2 ; do
+    if [ ! -f "SRR1216135_10k_${m}.fastq" ] ; then
+        python fastq_sample.py \
+            --in SRR1216135_100k_${m}.fastq \
+            --out SRR1216135_10k_${m}.fastq \
+            --seed 72436 \
+            --n 10000
+    fi
+done
