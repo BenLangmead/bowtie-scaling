d286f47379a81f64b7f86eace8378685c9200998
Christopher Wilks <cwilks3@jhu.edu>
2016-06-28 17:46:34 -0400

    changed find command to get the true max thread time; and added sorting

--- thread_scaling/scripts/master_tabulate.py
+++ thread_scaling/scripts/master_tabulate.py
@@ -5,53 +5,60 @@ Script for tabulating results from master.py
 from __future__ import print_function
 import subprocess
 import sys
+from operator import itemgetter
 
 if len(sys.argv) < 2:
     raise RuntimeError('Too few arguments')
 
 print('\t'.join(['experiment', 'run', 'tool', 'lock', 'version', 'sensitivity', 'paired', 'threads', 'seconds']))
 for mydr in sys.argv[1:]:
-    p = subprocess.Popen("find %s -name '*.txt' | xargs tail -n 1 | sed 's/.*: //'" % mydr,
+    p = subprocess.Popen("find %s -name '*.txt' | xargs grep \"thread:.*time\" | sed 's/:thread:.*time: /|/'" % mydr,
                          stdout=subprocess.PIPE, shell=True)
     dr = None
+    dedup = {}
     for ln in p.stdout:
-        if ln.startswith("==>"):
-            dr = ln.split()[1]
-        elif ':' in ln:
-            toks = map(float, ln.rstrip().split(':'))
-            secs = toks[-1]
-            secs += toks[-2] * 60
-            secs += toks[-3] * 60 * 60
-            exp, run, sens, pe, thr = dr.split('/')
-            thr = thr[:-4]
-            tool = 'hisat'
-            if run.startswith('bt2-'):
-                tool = 'bowtie2'
-            elif run.startswith('bt-'):
-                tool = 'bowtie'
-            lock = 'tinythreads fast_mutex'
-            if 'batch-tt' in run:
-                lock = 'batch tinythreads fast_mutex'
-            elif 'tbbpin-q' in run and 'rawseqs' in run:
-                lock = 'TBB queuing_mutex noio reads'
-            elif 'batch-tbbpin-q' in run:
-                lock = 'batch TBB queuing_mutex'
-            elif 'tbbpin-spin' in run:
-                lock = 'TBB spin_mutex'
-            elif 'tbbpin-heavy' in run:
-                lock = 'TBB mutex'
-            elif 'tbbpin-q' in run:
-                lock = 'TBB queuing_mutex'
-            elif 'tbbpin-co-q' in run:
-                lock = 'TBB/JHU CohortLock queue'
-            elif 'tbbpin-co-tktptl' in run:
-                lock = 'TBB/JHU CohortLock tktptl'
-            elif 'no-io' in run or 'noio' in run:
-                lock = 'None (stubbed I/O)'
-            version = 'Original parsing'
-            #since batch_parsing is built on cleanparse
-            if 'cleanparse' in run or 'batch' in lock:
-                version = 'Optimized parsing'
-            if 'norandom' in run:
-                version = "%s, No randomization" % (version)
-            print('\t'.join(map(str, [exp, run, tool, lock, version, sens, pe, thr, secs])))
+        (dr, time_) = ln.rstrip().split('|')
+        exp, run, sens, pe, thr = dr.split('/')
+        dr_ = '/'.join([exp, run, sens, pe])
+        thr = int(thr[:-4])
+        toks = map(float, time_.split(':'))
+        secs = toks[-1]
+        secs += toks[-2] * 60
+        secs += toks[-3] * 60 * 60
+        if dr in dedup:
+            if secs < dedup[dr][2]:
+                secs = dedup[dr][2]
+        dedup[dr] = [dr_, thr, secs]
+    for (dr_, thr, secs) in sorted(dedup.values(), key=itemgetter(0,1,2)):
+        exp, run, sens, pe = dr_.split('/')
+        tool = 'hisat'
+        if run.startswith('bt2-'):
+            tool = 'bowtie2'
+        elif run.startswith('bt-'):
+            tool = 'bowtie'
+        lock = 'tinythreads fast_mutex'
+        if 'batch-tt' in run:
+            lock = 'batch tinythreads fast_mutex'
+        elif 'tbbpin-q' in run and 'rawseqs' in run:
+            lock = 'TBB queuing_mutex noio reads'
+        elif 'batch-tbbpin-q' in run:
+            lock = 'batch TBB queuing_mutex'
+        elif 'tbbpin-spin' in run:
+            lock = 'TBB spin_mutex'
+        elif 'tbbpin-heavy' in run:
+            lock = 'TBB mutex'
+        elif 'tbbpin-q' in run:
+            lock = 'TBB queuing_mutex'
+        elif 'tbbpin-co-q' in run:
+            lock = 'TBB/JHU CohortLock queue'
+        elif 'tbbpin-co-tktptl' in run:
+            lock = 'TBB/JHU CohortLock tktptl'
+        elif 'no-io' in run or 'noio' in run:
+            lock = 'None (stubbed I/O)'
+        version = 'Original parsing'
+        #since batch_parsing is built on cleanparse
+        if 'cleanparse' in run or 'batch' in lock:
+            version = 'Optimized parsing'
+        if 'norandom' in run:
+            version = "%s, No randomization" % (version)
+        print('\t'.join(map(str, [exp, run, tool, lock, version, sens, pe, thr, secs])))
