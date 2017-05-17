06aadc5c497bc1fe9adf6fbaaa469b7524b61470
ChristopherWilks <broadsword@gmail.com>
2017-05-17 15:03:44 -0400

    adding additional split outs for filter-for-plotting script

--- thread_scaling/scripts/filter_tabulated_list.sh
+++ thread_scaling/scripts/filter_tabulated_list.sh
@@ -3,9 +3,25 @@
 #basic locks (no batch variations)
 egrep -v -e '-batch.*-' $1 | egrep -v -e '-bbatch.*-' | egrep -v -e '-cleanparse-' > ${1}.locks
 
-#parsing (batch variations)
+#parsing full
 echo 'experiment	run	tool	lock	version	sensitivity	paired	threads	seconds' > ${1}.parsing
-egrep -e 'batch' $1 | egrep -v -e 'heavy' | egrep -v -e 'spin' | egrep -v -e 'tt' >> ${1}.parsing
+egrep -e 'batch' $1 | egrep -v -e 'heavy' | egrep -v -e 'spin' | egrep -v -e 'tt' | egrep -v -e 'TBB queuing_mutex MP' >> ${1}.parsing
+
+#Bowtie2 vs. BWA
+echo 'experiment	run	tool	lock	version	sensitivity	paired	threads	seconds' > ${1}.bwa
+egrep -e 'bwa' $1 >> ${1}.bwa
+egrep -e 'input=32 & output=32' $1 | egrep -e 'queuing' | egrep -e 'bowtie2' | egrep -v -e 'MP' >> ${1}.bwa
+
+#memory
+echo 'experiment	run	tool	lock	version	sensitivity	paired	threads	seconds' > ${1}.mem
+egrep -e '-batch-tbb-q-id	' $1 >> ${1}.mem
+egrep -e 'TBB queuing_mutex' $1 | egrep -e 'Stack' | egrep -e 'Batch parsing input=32 & output=32' | egrep -v -e 'MP' | egrep -v -e 'Fixed' >> ${1}.mem
+
+
+#parsing for just queuelock and tthreads MP
+echo 'experiment	run	tool	lock	version	sensitivity	paired	threads	seconds' > ${1}.qlock_parsing
+egrep -e 'queuing' ${1}.parsing | egrep "Large Pool Stack" >> ${1}.qlock_parsing
+grep "MP" $1 | egrep -v -e 'Large Pool Stack' >> ${1}.qlock_parsing
 
 #baseline
 echo 'experiment	run	tool	lock	version	sensitivity	paired	threads	seconds' > ${1}.baseline
