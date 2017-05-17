0992fd5efd597408ec36130f0ec8d6050b11b627
Chris Wilks <broadsword@gmail.com>
2017-05-17 16:24:10 -0400

    worked out final filtering for baseline, parsing, and final plots

--- results/filter_result_table_for_plots.sh
+++ results/filter_result_table_for_plots.sh
@@ -1,29 +1,31 @@
 #!/bin/bash
 
-#basic locks (no batch variations)
-egrep -v -e '-batch.*-' $1 | egrep -v -e '-bbatch.*-' | egrep -v -e '-cleanparse-' > ${1}.locks
+#baseline: TT, TBB locks, MP, MP+MT (TTthreads) (no batch variations)
+egrep -v -e '-batch.*-' $1 | egrep -v -e '-bbatch.*-' | egrep -v -e '-cleanparse-' | egrep -v -e 'bwa' > ${1}.baseline.tsv
 
-#parsing full
-echo 'experiment	run	tool	lock	version	sensitivity	paired	threads	seconds' > ${1}.parsing
-egrep -e 'batch' $1 | egrep -v -e 'heavy' | egrep -v -e 'spin' | egrep -v -e 'tt' | egrep -v -e 'TBB queuing_mutex MP' >> ${1}.parsing
+#parsing with just queuelock and tthreads MP (across all batch modes)
+echo 'experiment	run	tool	lock	version	sensitivity	paired	threads	seconds' > ${1}.parsing.tsv
+egrep -e 'batch' $1 | egrep -v -e 'heavy' | egrep -v -e 'spin' | egrep -v -e 'MP' | egrep -v -e 'tt' | egrep -v -e 'TBB queuing_mutex MP' >> ${1}.parsing
+egrep -e 'queuing' ${1}.parsing | egrep "Stack" >> ${1}.parsing.tsv
+egrep -e 'TBB queuing_mutex' ${1} | egrep -e 'Original parsing' >> ${1}.parsing.tsv
+rm ${1}.parsing
+
+#final: Queuing Batch=32 (in/out) Stack/mem, Queuing Batch=32 (in/out) Stack/mem w Fixed, MP, MP+MT, BWA (BT2)
+echo 'experiment	run	tool	lock	version	sensitivity	paired	threads	seconds' > ${1}.final.tsv
+egrep -e 'TBB queuing_mutex' $1 | egrep -e 'Batch parsing input=32 & output=32' | egrep -e 'Stack' | egrep -v -e 'MP' >> ${1}.final.tsv
+#includes MP+MT
+grep "MP" $1 | egrep -e 'Original parsing' >> ${1}.final.tsv
+egrep -e 'bwa' $1 >> ${1}.final.tsv
 
 #Bowtie2 vs. BWA
-echo 'experiment	run	tool	lock	version	sensitivity	paired	threads	seconds' > ${1}.bwa
-egrep -e 'bwa' $1 >> ${1}.bwa
-egrep -e 'input=32 & output=32' $1 | egrep -e 'queuing' | egrep -e 'bowtie2' | egrep -v -e 'MP' >> ${1}.bwa
+#echo 'experiment	run	tool	lock	version	sensitivity	paired	threads	seconds' > ${1}.bwa
+#egrep -e 'bwa' $1 >> ${1}.bwa
+#egrep -e 'input=32 & output=32' $1 | egrep -e 'queuing' | egrep -e 'bowtie2' | egrep -v -e 'MP' >> ${1}.bwa
 
 #memory
-echo 'experiment	run	tool	lock	version	sensitivity	paired	threads	seconds' > ${1}.mem
-egrep -e '-batch-tbb-q-id	' $1 >> ${1}.mem
-egrep -e 'TBB queuing_mutex' $1 | egrep -e 'Stack' | egrep -e 'Batch parsing input=32 & output=32' | egrep -v -e 'MP' | egrep -v -e 'Fixed' >> ${1}.mem
+#echo 'experiment	run	tool	lock	version	sensitivity	paired	threads	seconds' > ${1}.mem
+#egrep -e '-batch-tbb-q-id	' $1 >> ${1}.mem
+#egrep -e 'TBB queuing_mutex' $1 | egrep -e 'Stack' | egrep -e 'Batch parsing input=32 & output=32' | egrep -v -e 'MP' | egrep -v -e 'Fixed' >> ${1}.mem
 
 
-#parsing for just queuelock and tthreads MP
-echo 'experiment	run	tool	lock	version	sensitivity	paired	threads	seconds' > ${1}.qlock_parsing
-egrep -e 'queuing' ${1}.parsing | egrep "Large Pool Stack" >> ${1}.qlock_parsing
-grep "MP" $1 | egrep -v -e 'Large Pool Stack' >> ${1}.qlock_parsing
-
-#baseline
-echo 'experiment	run	tool	lock	version	sensitivity	paired	threads	seconds' > ${1}.baseline
-egrep -e 'tinythreads fast_mutex' $1 | egrep -v -e '-batch.*-' | egrep -v -e 'cleanparse' >> ${1}.baseline
-#egrep -e 'MP-MT' $1 >> ${1}.baseline
+#grep "MP" $1 | egrep -v -e 'Large Pool Stack' >> ${1}.qlock_parsing
