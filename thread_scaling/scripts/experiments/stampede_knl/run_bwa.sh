990e1316773633425cbd1a57c6c1f387026ad24b
ChristopherWilks <broadsword@gmail.com>
2017-05-19 01:50:50 -0500

    switched bwa input file to use extended reads and added THREAD_SERIES var

--- thread_scaling/scripts/experiments/stampede_knl/run_bwa.sh
+++ thread_scaling/scripts/experiments/stampede_knl/run_bwa.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
+THREAD_SERIES="1,4,8,12,16,17,34,51,68,85,100,102,119,136,150,153,170,200,204,221,238,255,272"
+
 BWA_CMD=/home1/04620/cwilks/bwa
 OUTDIR="bwa0.7.15"
 
 export ROOT1=/work/04620/cwilks/data
 export ROOT2=/tmp
-rsync -av $ROOT1/ERR050082_1.fastq.shuffled2.fq.block $ROOT2/
+rsync -av $ROOT1/ERR050082_1.fastq.shuffled2_extended.fq.block $ROOT2/
 rsync -av $ROOT1/ERR050082_2.fastq.shuffled2.fq.block $ROOT2/
-export READS1=$ROOT2/ERR050082_1.fastq.shuffled2.fq.block
+export READS1=$ROOT2/ERR050082_1.fastq.shuffled2_extended.fq.block
 export READS2=$ROOT2/ERR050082_1.fastq.shuffled2.fq.block
 
 export INDEX_ROOT=/dev/shm
@@ -15,7 +17,7 @@ rsync -av $ROOT1/hg19.fa* $INDEX_ROOT/
 
 
 #single
-python master_external.py --cmd "$BWA_CMD mem" --genome $INDEX_ROOT/hg19.fa --U $READS1 --nthread-series 1,4,8,12,16,17,34,51,68,85,100,102,119,136,150,153,170,200,204,221,238,255,272  --tempdir $ROOT2 --output-dir ./${1}/$OUTDIR/sensitive/unp --reads-per-thread 12500
+python master_external.py --cmd "$BWA_CMD mem" --genome $INDEX_ROOT/hg19.fa --U $READS1 --nthread-series $THREAD_SERIES  --tempdir $ROOT2 --output-dir ./${1}/$OUTDIR/sensitive/unp --reads-per-thread 12500
 
 #paired
-python master_external.py --cmd "$BWA_CMD mem" --genome $INDEX_ROOT/hg19.fa --U $READS1 --U2 $READS2 --nthread-series 1,4,8,12,16,17,34,51,68,85,100,102,119,136,150,153,170,200,204,221,238,255,272  --tempdir $ROOT2 --output-dir ./${1}/$OUTDIR/sensitive/pe --reads-per-thread 18000
+python master_external.py --cmd "$BWA_CMD mem" --genome $INDEX_ROOT/hg19.fa --U $READS1 --U2 $READS2 --nthread-series $THREAD_SERIES  --tempdir $ROOT2 --output-dir ./${1}/$OUTDIR/sensitive/pe --reads-per-thread 18000
