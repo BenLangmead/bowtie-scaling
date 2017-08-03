8d027a29e4632f1e72d1d1a05fa0f5c893b64024
Ben Langmead <ben.langmead@gmail.com>
2017-08-02 20:48:05 -0400

    break out 100 and 50; add seed

--- thread_scaling/scripts/reads.sh
+++ thread_scaling/scripts/reads.sh
@@ -10,23 +10,35 @@ for i in `cat .reads.txt` ; do
 done
 
 for i in 0 1 2 3 4 5 6 7 8 9 ; do
-    cat >.reads.${i}.sh <<EOF
+    cat >.reads100.${i}.sh <<EOF
 #!/bin/bash -l
 #SBATCH
 #SBATCH --partition=shared
 #SBATCH --nodes=1
 #SBATCH --mem=4G
-#SBATCH --time=8:00:00
+#SBATCH --time=10:00:00
 #SBATCH --ntasks-per-node=1
 
 # For HISAT unpaired to run about a minute, we need about 300M reads
-python reads.py --prefix=mix100_${i} --temp-dir=mix100_${i}_temp --reads-per-accession 10000000
-
-# For Bowtie 1 unpaired to run about a mnute, we need about 300M reads
-python reads.py --trim-to 50 --max-read-size 175 --prefix=mix50_${i} --temp-dir=mix50_${i}_temp --reads-per-accession 10000000
+python reads.py --prefix=mix100_${i} --temp-dir=mix100_${i}_temp --reads-per-accession 10000000 --keep-intermediates --resume --seed ${i}
 
 EOF
-    echo "sbatch .reads.${i}.sh"
+    echo "sbatch .reads100.${i}.sh"
+
+    cat >.reads50.${i}.sh <<EOF
+#!/bin/bash -l
+#SBATCH
+#SBATCH --partition=shared
+#SBATCH --nodes=1
+#SBATCH --mem=4G
+#SBATCH --time=10:00:00
+#SBATCH --ntasks-per-node=1
+
+# For Bowtie 1 unpaired to run about a mnute, we need about 300M reads
+python reads.py --trim-to 50 --max-read-size 175 --prefix=mix50_${i} --temp-dir=mix50_${i}_temp --reads-per-accession 10000000 --keep-intermediates --resume --seed ${i}
+
+EOF
+    echo "sbatch .reads50.${i}.sh"
 done
 
 #for i in `cat .reads.txt` ; do
