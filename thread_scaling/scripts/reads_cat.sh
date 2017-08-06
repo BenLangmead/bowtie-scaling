2bde4e56671ccae09a0b84c0824e4db955abae6f
Ben Langmead <ben.langmead@gmail.com>
2017-08-06 15:29:12 -0400

    fix reads cat script

--- thread_scaling/scripts/reads_cat.sh
+++ thread_scaling/scripts/reads_cat.sh
@@ -2,16 +2,20 @@
 
 for m in 1 2 ; do
     for l in 50 100 ; do
-	cat >.zip${l}_${m}.sh <<EOF
+	for b in "" "_block" ; do
+	    if [ ! -f "mix${l}${b}_${m}.fq.gz" ] ; then
+		cat >.zip${l}${b}_${m}.sh <<EOF
 #!/bin/bash -l
 #SBATCH
 #SBATCH --partition=shared
 #SBATCH --nodes=1
 #SBATCH --mem=1G
-#SBATCH --time=4:00:00
+#SBATCH --time=12:00:00
 #SBATCH --ntasks-per-node=1
-cat mix${l}_?_${m}.fq | gzip -c9 > mix${l}_${m}.fq.gz
+cat mix${l}_?${b}_${m}.fq | grep -v '^$' | gzip -c9 > mix${l}_?${b}_${m}.fq.gz
 EOF
-	echo "sbatch .zip${l}_${m}.sh"
+		echo "sbatch .zip${l}${b}_${m}.sh"
+	    fi
+	done
     done
 done
