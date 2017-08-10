8c733ca48f6aa43721a61402a95eccbc4579d22f
BenLangmead <langmea@cs.jhu.edu>
2017-08-10 10:11:42 -0400

    fix cat script to get at bucket boundary

--- thread_scaling/scripts/reads_cat.sh
+++ thread_scaling/scripts/reads_cat.sh
@@ -1,10 +1,16 @@
 #!/bin/sh
 
+BLOCK_BYTES=12288
+
 for m in 1 2 ; do
-    for l in 50 100 ; do
-	for b in "" "_block" ; do
-	    if [ ! -f "mix${l}${b}_${m}.fq.gz" ] ; then
-		cat >.zip${l}${b}_${m}.sh <<EOF
+	for l in 50 100 ; do
+		READS_PER_BLOCK=70
+		if [ "${l}" = "100" ] ; then
+			READS_PER_BLOCK=44
+		fi
+		for b in "" "_block" ; do
+			if [ ! -f "mix${l}${b}_${m}.fq.gz" ] ; then
+				cat >.zip${l}${b}_${m}.sh <<EOF
 #!/bin/bash -l
 #SBATCH
 #SBATCH --partition=shared
@@ -12,10 +18,25 @@ for m in 1 2 ; do
 #SBATCH --mem=1G
 #SBATCH --time=12:00:00
 #SBATCH --ntasks-per-node=1
-cat mix${l}_?${b}_${m}.fq | grep -v '^$' | gzip -c9 > mix${l}${b}_${m}.fq.gz
+
+set -ex
+
+echo > mix${l}${b}_${m}.fq
+
+for fn in mix${l}_?${b}_${m}.fq ; do
+    if echo ${fn} | grep -q block ; then
+        L=`wc -c \${fn} | awk '{print \$1}'`
+        HEADC=`python -c "print (\$L - (\$L % $BLOCK_BYTES))"`
+        head -c $HEADC ${fn} >> mix${l}${b}_${m}.fq
+    else
+        cat ${fn} >> mix${l}${b}_${m}.fq
+    fi
+done
+
+pypy check_blocked.py --fastq mix${l}${b}_${m}.fq --block-bytes $BLOCK_BYTES --reads-per-block $READS_PER_BLOCK
 EOF
-		echo "sbatch .zip${l}${b}_${m}.sh"
-	    fi
+				echo "sbatch .zip${l}${b}_${m}.sh"
+			fi
+		done
 	done
-    done
 done
