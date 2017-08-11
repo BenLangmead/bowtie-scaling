e3e853d1fcaac48c4f5bf63a912ad000757c0e62
BenLangmead <langmea@cs.jhu.edu>
2017-08-11 15:36:45 -0400

    fixes read cat'ing so files have exact same reads

--- thread_scaling/scripts/reads_cat.sh
+++ thread_scaling/scripts/reads_cat.sh
@@ -8,9 +8,8 @@ for m in 1 2 ; do
 		if [ "${l}" = "100" ] ; then
 			READS_PER_BLOCK=44
 		fi
-		for b in "" "_block" ; do
-			if [ ! -f "mix${l}${b}_${m}.fq.gz" ] ; then
-				cat >.zip${l}${b}_${m}.sh <<EOF
+		if [ ! -f "mix${l}${b}_${m}.fq.gz" ] ; then
+			cat >.zip${l}${b}_${m}.sh <<EOF
 #!/bin/bash -l
 #SBATCH
 #SBATCH --partition=shared
@@ -21,22 +20,26 @@ for m in 1 2 ; do
 
 set -ex
 
-echo > mix${l}${b}_${m}.fq
+rm -f mix${l}_block_${m}.fq mix${l}_${m}.fq
+touch mix${l}_block_${m}.fq mix${l}_${m}.fq
 
-for fn in mix${l}_?${b}_${m}.fq ; do
-    if echo ${fn} | grep -q block ; then
-        L=`wc -c \${fn} | awk '{print \$1}'`
-        HEADC=`python -c "print (\$L - (\$L % $BLOCK_BYTES))"`
-        head -c $HEADC ${fn} >> mix${l}${b}_${m}.fq
-    else
-        cat ${fn} >> mix${l}${b}_${m}.fq
-    fi
+for bfn in mix${l}_?_block_${m}.fq ; do
+    fn=`echo \${bfn} | sed 's/_block//'`
+    BYTES=`wc -c \${bfn} | awk '{print \$1}'`
+    HEADC=`python -c "print (\$BYTES - (\$BYTES % $BLOCK_BYTES))"`
+    LINES=`head -c $HEADC \${bfn} | wc -l`
+    # make sure we're taking the same (and same # of) reads from both
+    head -c $HEADC \${bfn} >> mix${l}_block_${m}.fq
+    head -n $LINES \${fn}  >> mix${l}_${m}.fq
 done
 
-pypy check_blocked.py --fastq mix${l}${b}_${m}.fq --block-bytes $BLOCK_BYTES --reads-per-block $READS_PER_BLOCK
+# check the blocked output to ensure boundaries are correct
+pypy check_blocked.py --fastq mix${l}_block_${m}.fq \
+                      --block-bytes $BLOCK_BYTES \
+                      --reads-per-block $READS_PER_BLOCK
 EOF
-				echo "sbatch .zip${l}${b}_${m}.sh"
-			fi
-		done
+			echo "sbatch .zip${l}${b}_${m}.sh"
+		fi
 	done
 done
+
