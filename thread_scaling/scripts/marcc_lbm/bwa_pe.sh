9f21993045d6ae8e7f412659c2a402c1f55cf3fb
BenLangmead <langmea@cs.jhu.edu>
2017-08-16 16:04:49 -0400

    make # reads equal between BWA and Bowtie 2 experiments

--- thread_scaling/scripts/marcc_lbm/bwa_pe.sh
+++ thread_scaling/scripts/marcc_lbm/bwa_pe.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -l
 
 d=`dirname $PWD`
-sh $d/common.sh bwa bwa.tsv marcc_lbm pe 50000
+sh $d/common.sh bwa bwa.tsv marcc_lbm pe 85000
--- thread_scaling/scripts/marcc_lbm/bwa_unp.sh
+++ thread_scaling/scripts/marcc_lbm/bwa_unp.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -l
 
 d=`dirname $PWD`
-sh $d/common.sh bwa bwa.tsv marcc_lbm unp 120000
+sh $d/common.sh bwa bwa.tsv marcc_lbm unp 200000
