3c3594a77c08c51ecdfbd8ada35921d150420121
BenLangmead <langmea@cs.jhu.edu>
2017-10-23 20:56:27 -0400

    removing old system directories

--- thread_scaling/scripts/marcc/run_lbm.sh
+++
@@ -1,3 +0,0 @@
-./bt1_pub_lbm.sh pub_reshuf1_lbm > bt1.run.lbm 2>&1
-./hisat_pub_lbm.sh pub_reshuf1_lbm > hisat.run.lbm 2>&1
-./bt2_pub_lbm.sh pub_reshuf1_lbm > bt2.run.lbm 2>&1
--- thread_scaling/scripts/old/shorten.awk
+++
@@ -1,9 +0,0 @@
-{ ln += 1 }
-
-{
-  if((ln % 4) == 2 || (ln % 4) == 0 || (ln % 4) == 1) {
-    print substr($0, 0, 50)
-  } else {
-    print
-  }
-}
