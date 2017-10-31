e0e693a9136ac210a5510878420fbc0583e78b82
BenLangmead <langmea@cs.jhu.edu>
2017-10-31 15:47:54 -0400

    revamp all script

--- thread_scaling/scripts/stampede_knl/all.sh
+++ thread_scaling/scripts/stampede_knl/all.sh
@@ -1,6 +1,15 @@
 #!/bin/sh
 
-for i in bt*.sh ht*.sh ; do 
-    echo $i
-    sbatch $i
+ALIGNERS="bt bt2 ht"
+ENDS="unp pe"
+TYPES="base batchlo batchhi batchinbuf lustre"
+
+for al in ${ALIGNERS} ; do
+    for typ in ${TYPES} ; do
+        for en in ${ENDS} ; do
+            FN="${al}_${typ}_${en}.sh"
+            echo ${FN}
+            sbatch ${FN}
+        done
+    done
 done
