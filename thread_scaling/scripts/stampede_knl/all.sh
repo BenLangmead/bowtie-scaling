ebb727aa6f36dcd5608d8f4415780c8246473510
BenLangmead <langmea@cs.jhu.edu>
2017-11-01 08:43:36 -0400

    driver script

--- thread_scaling/scripts/stampede_knl/all.sh
+++ thread_scaling/scripts/stampede_knl/all.sh
@@ -1,15 +1,21 @@
 #!/bin/sh
 
 ALIGNERS="bt bt2 ht"
+DO_BWA=1
 ENDS="unp pe"
 TYPES="base batchlo batchhi batchinbuf lustre"
 
-for al in ${ALIGNERS} ; do
-    for typ in ${TYPES} ; do
-        for en in ${ENDS} ; do
+for en in ${ENDS} ; do
+    for al in ${ALIGNERS} ; do
+        for typ in ${TYPES} ; do
             FN="${al}_${typ}_${en}.sh"
             echo ${FN}
             sbatch ${FN}
         done
     done
+    if [ "${DO_BWA}" = "1" ] ; then
+        FN="bwa_${en}.sh"
+        echo ${FN}
+        sbatch ${FN}
+    fi
 done
