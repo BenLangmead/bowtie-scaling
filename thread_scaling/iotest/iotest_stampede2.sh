d0331a4b23b4f76bdcbd3b61064a7d77d29d8a9f
Ben Langmead <langmea@cs.jhu.edu>
2017-12-19 21:38:28 -0500

    fix missing exprs

--- thread_scaling/iotest/iotest_stampede2.sh
+++ thread_scaling/iotest/iotest_stampede2.sh
@@ -3,7 +3,7 @@
 # iotest -l <lo block size> -h <hi block size> -s <step size> -b <bytes> -o <output file>
 
 echo "Writing 16GB file with block sizes from 512K-2GB"
-SZ=`16 \* 1024 \* 1024`
+SZ=`expr 16 \* 1024 \* 1024`
 LO=`expr 512 \* 1024`
 HI=`expr 2 \* 1024 \* 1024`
 STEP=`expr 512 \* 1024`
@@ -11,9 +11,10 @@ IOTEST="./iotest -l ${LO} -s ${STEP} -h ${HI}"
 
 for mp in 1 2 4 ; do
 
-    sz_mp=`${SZ} / ${mp}`
+    sz_mp=`expr ${SZ} / ${mp}`
 
     echo "${mp} processes, ${sz_mp} bytes each"
+    pids=""
 
     echo "  stubbed out (/dev/null)"
     for i in `seq 1 ${mp}` ; do
