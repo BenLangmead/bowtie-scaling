3534b7f201240d0680569c6ce271742b7cb3b73c
Ben Langmead <langmea@cs.jhu.edu>
2017-12-19 21:33:27 -0500

    add MP to iotest

--- thread_scaling/iotest/iotest_stampede2.sh
+++ thread_scaling/iotest/iotest_stampede2.sh
@@ -1,12 +1,51 @@
 #!/bin/sh
 
-echo "Writing 1GB file with block sizes from 512K-2GB"
+# iotest -l <lo block size> -h <hi block size> -s <step size> -b <bytes> -o <output file>
 
-echo "Stubbed out (/dev/null)"
-./iotest -l `expr 512 \* 1024` -s `expr 512 \* 1024` -h `expr 2 \* 1024 \* 1024` -b 1073741824 -o /dev/null
+echo "Writing 16GB file with block sizes from 512K-2GB"
+SZ=`16 \* 1024 \* 1024`
+LO=`expr 512 \* 1024`
+HI=`expr 2 \* 1024 \* 1024`
+STEP=`expr 512 \* 1024`
+IOTEST="./iotest -l ${LO} -s ${STEP} -h ${HI}"
 
-echo "SSD out (/tmp)"
-./iotest -l `expr 512 \* 1024` -s `expr 512 \* 1024` -h `expr 2 \* 1024 \* 1024` -b 1073741824 -o /tmp/test1.img
+for mp in 1 2 4 ; do
 
-echo "Lustre out (\$SCRATCH)"
-./iotest -l `expr 512 \* 1024` -s `expr 512 \* 1024` -h `expr 2 \* 1024 \* 1024` -b 1073741824 -o /scratch/04265/benbo81/ost1/test1.img
+    sz_mp=`${SZ} / ${mp}`
+
+    echo "${mp} processes, ${sz_mp} bytes each"
+
+    echo "  stubbed out (/dev/null)"
+    for i in `seq 1 ${mp}` ; do
+        ${IOTEST} -b ${sz_mp} -o /dev/null &
+        pids="${pids} $!"
+    done
+    for pid in ${pids} ; do
+        wait ${pid}
+    done
+    pids=""
+
+    echo "  SSD out (/tmp)"
+    for i in `seq 1 ${mp}` ; do
+        ${IOTEST} -b ${sz_mp} -o "/tmp/test${i}.img" &
+        pids="${pids} $!"
+    done
+    for pid in ${pids} ; do
+        wait ${pid}
+    done
+    pids=""
+
+    for ost in 1 16 ; do
+        dr="/scratch/04265/benbo81/ost${ost}"
+        nstripes=`lfs getstripe -c ${dr}`
+        echo "  Lustre out (\$SCRATCH) (${nstripes} OST stripes)"
+        for i in `seq 1 ${mp}` ; do
+            ${IOTEST} -b ${sz_mp} -o "${dr}/test${i}.img" &
+            pids="${pids} $!"
+        done
+        for pid in ${pids} ; do
+            wait ${pid}
+        done
+        pids=""
+    done
+done
