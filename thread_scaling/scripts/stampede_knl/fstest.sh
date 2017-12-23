a7aebf2d98866bf44dd03993c6028d340bb3e5c5
Ben Langmead <ben.langmead@gmail.com>
2017-12-22 21:15:25 -0600

    working version

--- thread_scaling/scripts/stampede_knl/fstest.sh
+++ thread_scaling/scripts/stampede_knl/fstest.sh
@@ -42,30 +42,37 @@ align() {
     input_dir=$2
     output_dir=$3
     mp=$4
+    mpm1=$((${mp} - 1))
     mt=$5
-    rm -f .fstest.sh.times.*
     ofs=""
-    for i in `seq 1 ${mp}` ; do
+    for i in `seq 0 ${mpm1}` ; do
         ii=`printf "%02d" ${i}`
         of=${output_dir}
         if [ ${of} != "/dev/null" ] ; then
             of="${output_dir}/${mp}_${ii}.sam"
             ofs="$ofs $of"
+	    echo blah > ${of}
+	    test -f ${of}
+	    rm -f ${of}
         fi
+	test -f ${input_dir}/1_${mp}_${ii}
+	test -f ${input_dir}/2_${mp}_${ii}
         ${SCR_DIR}/bin/hisat-align-s-${ver} \
             -p ${mt} -I 250 -X 800 --reads-per-batch 32 \
                 --block-bytes 12288 --reads-per-block 44 \
                 --no-spliced-alignment --no-temp-splicesite \
                 -x ${INDEXES}/hisat/hg38 -t \
                 -1 ${input_dir}/1_${mp}_${ii} -2 ${input_dir}/2_${mp}_${ii} \
-                -S ${of} 2>/dev/null | \
-                    awk -v FS=':' '$1 == "thread" && $2 ~ /time/ {print($2" "$3*60*60+$4*60+$5)}' | \
-                    awk '{print $NF}' > .fstest.sh.times.$i &
-        pids="${pids} $!"
+                -S ${of} >.fstest.sh.out.$i 2>/dev/null &
     done
-    for pid in ${pids} ; do wait ${pid} ; done
+    wait
+    for i in `seq 0 ${mpm1}` ; do
+	cat .fstest.sh.out.$i | \
+	    awk -v FS=':' '$1 == "thread" && $2 ~ /time/ {print($2" "$3*60*60+$4*60+$5)}' | \
+	    awk '{print $NF}' > .fstest.sh.times.$i
+    done
     cat .fstest.sh.times.* | st | sed "s/^/${1} /"
-    rm -f ${ofs} .fstest.sh.times.* 
+    rm -f ${ofs} .fstest.sh.times.* .fstest.sh.out.*
 }
 
 for mp in ${MP_LIST} ; do
@@ -86,9 +93,9 @@ for mp in ${MP_LIST} ; do
             
             for outd in /dev/null /tmp ${INPUT_01OST} ${INPUT_16OST} ; do
                 mt=`expr ${MT_MAX} / ${mp}`
-                echo "Input: ${ind} output: ${outd} mp: ${mp} mt: ${mt}"
+                echo "Input: ${d} output: ${outd} mp: ${mp} mt: ${mt}"
                 for i in `seq 1 ${NTRIALS}` ; do
-                    align "${VERSION}" "${ind}" "${outd}" "${mp}" "${mt}"
+                    align "${VERSION}" "${d}" "${outd}" "${mp}" "${mt}"
                 done
             done
         done
